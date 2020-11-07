f5f41bbb7a7c8f362e540aa806b5f40fd958cb85
William Jackson <williampj@gmail.com>
2020-11-06 19:52:33 -0600

    updated controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,21 @@
 class SessionsController < ApplicationController
-  def login 
-
+  def create  
+    user = User.find_by_email(params[:email])
+    if user && user.password == params[:password]
+      session[:user_id] = user.id 
+      render json: @user, status: :logged_in
+    else 
+      render json: 
   end
 
-  def logout 
-    
+  def destroy   
+    # Delete session
+    # return a confirmation 
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:email, :password)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,22 @@
-require 'bcrypt'
-require 'pry'
+# require 'bcrypt'
+# require 'pry'
 
 class UsersController < ApplicationController
   # has_secure_password 
   
   def create 
-    username = params[:username]
+    email = params[:email]
     password = params[:password]
-    password_hash = BCrypt::Password.create(password)
-
-    binding.pry 
+    # password_hash = BCrypt::Password.create(password)
+    
+    new_user = User.new(user_params)
+    if new_user.save  
+      render json: @user, status: :created
+    else 
+      render json: @user.errors, status: :unprocessable_entity
+    end
+    # puts password_hash 
+    # binding.pry 
     # we need a password from the params (params[:password])
     # we need a username from the params (params[:email]) 
     # Need to make those strong parameters! 
@@ -29,5 +36,11 @@ class UsersController < ApplicationController
     # Validate the user 
     # hash the password
     # The set username and password to the user and save to database
+  end
+
+  private 
+
+  def user_params 
+    params.require(:user).permit(:email, :password)
   end
 end
