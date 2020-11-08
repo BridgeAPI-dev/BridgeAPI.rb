3eaa80c111e5bf159c92d2c7aa48a78aa191b91e
William Jackson <williampj@gmail.com>
2020-11-08 12:34:59 -0600

    edited User model and table migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-    
+  attr_accessor :password_digest, :recovery_password_digest
+  has_secure_password :password 
+  has_secure_password :recovery_password, validations: false  
+  validates :email, presence: true, uniqueness: true 
+  validates :password, :password_digest, presence: true
 end
+
