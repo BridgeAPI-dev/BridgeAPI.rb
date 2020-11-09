c52ddb04010b253b3eed5df84a809b0e8071aa84
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-09 10:03:57 -0700

    Started work on bridge controller

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -1,2 +1,23 @@
-class BridgesController < ApplicationController 
+class BridgesController < ApplicationController
+  before_action :set_bridge, only: [:show]
+  
+  def index 
+    render json: Bridge.all
+  end
+
+  def show
+    if (@bridge)
+      render json: @bridge.to_json(include: [:headers, :environment_variables, :events])
+    else
+      render json: {}
+    end
+  end
+
+  protected
+  def bridge_params
+  end
+
+  def set_bridge
+    @bridge = Bridge.includes(:headers, :environment_variables, :events).find_by_id(params[:id])
+  end
 end
