af92988891fe8ac704ea910dbc03a34d978fc6d3
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-09 11:38:39 -0700

    added create, update, and destroy methods to bridges controller

--- app/controllers/bridges_controller.rb
+++ app/controllers/bridges_controller.rb
@@ -1,23 +1,48 @@
 class BridgesController < ApplicationController
-  before_action :set_bridge, only: [:show]
+  before_action :set_bridge, only: [:show, :update, :destroy]
   
   def index 
     render json: Bridge.all
   end
 
   def show
-    if (@bridge)
-      render json: @bridge.to_json(include: [:headers, :environment_variables, :events])
-    else
+    render json: @bridge.to_json(include: [:headers, :environment_variables, :events])
+  end
+
+  def create
+    @bridge = Bridge.new(bridge_params)
+    @bridge.inbound_url = generate_inbound_url
+    if @bridge.save
       render json: {}
+    else
+      render json: @bridge.errors, status: :internal_server_error
     end
+  end
+
+  def update
+    if @bridge.update bridge_params
+      render json: {}
+    else
+      render json: @bridge.errors, status: :internal_server_error
+    end
+  end
+
+  def destroy
+    @bridge.destroy
+    render json: {}, status: :deleted
   end
 
   protected
   def bridge_params
+    params.require(:bridge).permit(:name, :method, :retries, :delay, :outbound_url, :payload)
   end
 
   def set_bridge
     @bridge = Bridge.includes(:headers, :environment_variables, :events).find_by_id(params[:id])
+    render json: {}, status: :unprocessable_entity unless @bridge
+  end
+
+  def generate_inbound_url
+    "https://bridgeapi.io/b12324/inbound"
   end
 end
