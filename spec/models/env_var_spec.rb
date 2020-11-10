01c609f15d1180b70c56468d5f39eea11b923789
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-10 08:18:31 -0700

    removed unnecessary controllers and cleaned up tests

--- spec/models/env_var_spec.rb
+++ spec/models/env_var_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe EnvironmentVariable, type: :model do
-  it 'belongs to bridge' do
-    bridge = Bridge.create(name: 'My First Bridge', payload: '', inbound_url: 'https://bridgeapi.dev/b1234/inbound', outbound_url: 'https://wowservice.io/new/23847923864', method: 'post', retries: 5, delay: 15)
+  subject { 
+    Bridge.create(
+      name: 'bridge',
+      payload: '',
+      inbound_url: Bridge.generate_inbound_url,
+      outbound_url: "doggoapi.io/" + Bridge.generate_inbound_url, 
+      method: 'POST', 
+      retries: 5, 
+      delay: 15 
+    )
+  }
 
+  it 'belongs to bridge' do
     EnvironmentVariable1 = EnvironmentVariable.create(key: 'database', value: 'a102345ij2')
     EnvironmentVariable2 = EnvironmentVariable.create(key: 'database_password', value: 'supersecretpasswordwow')
-    bridge.env_vars << EnvironmentVariable1
-    bridge.env_vars << EnvironmentVariable2
-    expect(EnvironmentVariable1.bridge).to eq bridge 
-    expect(EnvironmentVariable2.bridge).to eq bridge 
+    subject.env_vars << EnvironmentVariable1
+    subject.env_vars << EnvironmentVariable2
+    expect(EnvironmentVariable1.bridge).to eq subject 
+    expect(EnvironmentVariable2.bridge).to eq subject 
   end
 end
