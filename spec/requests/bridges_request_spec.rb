9ee72eff41fa0f8687bf7fa02143e10b83ef949b
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-12 10:46:26 -0700

    rubocop fixes

--- spec/requests/bridges_request_spec.rb
+++ spec/requests/bridges_request_spec.rb
@@ -64,15 +64,15 @@ RSpec.describe 'Bridges', type: :request do
       @other_user.destroy!
     end
 
-    it 'destroys bridges' do 
-      subject.save! 
+    it 'destroys bridges' do
+      subject.save!
 
       delete bridge_path(subject.id), headers: authenticated_token
 
       expect(response).to be_successful
     end
 
-    it 'doesnt destroy other\'s bridges' do 
+    it 'doesnt destroy other\'s bridges' do
       create_other_user
       other_bridge = create_bridge
       other_bridge.name = 'other\s bridge'
@@ -84,22 +84,22 @@ RSpec.describe 'Bridges', type: :request do
       expect(response).to_not be_successful
     end
 
-    it 'updates bridges' do 
-      subject.save! 
+    it 'updates bridges' do
+      subject.save!
 
-      patch bridge_path(subject.id), params: { bridge: { name: 'updated bridge '}}, headers: authenticated_token
+      patch bridge_path(subject.id), params: { bridge: { name: 'updated bridge ' } }, headers: authenticated_token
 
       expect(response).to be_successful
     end
 
-    it 'doesnt update other\'s bridges' do 
+    it 'doesnt update other\'s bridges' do
       create_other_user
       other_bridge = create_bridge
       other_bridge.name = 'other\s bridge'
       other_bridge.user = @other_user
       other_bridge.save!
 
-      patch bridge_path(other_bridge.id), params: { bridge: { name: 'updated bridge '}}, headers: authenticated_token
+      patch bridge_path(other_bridge.id), params: { bridge: { name: 'updated bridge ' } }, headers: authenticated_token
 
       expect(response).to_not be_successful
     end
