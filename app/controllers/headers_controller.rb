3ad07c0f6a5c88ddce2f9928079b5771eea6bfd3
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-13 15:12:38 -0700

    added delete endpoints for headers and environment variables

---
+++ app/controllers/headers_controller.rb
@@ -0,0 +1,16 @@
+class HeadersController < ApplicationController
+  before_action :authorize_request
+  before_action :set_header
+
+  def destroy
+    @header.destroy
+    render_message
+  end
+
+  protected
+
+  def set_header
+    @header = Header.find_by(id: params[:id])
+    render_message status: :unprocessable_entity unless @header&.bridge.user == @current_user
+  end
+end
