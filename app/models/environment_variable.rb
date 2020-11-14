7ef2e208af70392caa1e720325280e3bfb4e71b4
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-11-13 20:47:38 -0700

    Started encryption. Value is automatically encrypted when changed.

--- app/models/environment_variable.rb
+++ app/models/environment_variable.rb
@@ -5,4 +5,31 @@ class EnvironmentVariable < ApplicationRecord
   validates :value, presence: true
 
   belongs_to :bridge
+
+  before_save :encrypt, if: Proc.new { |c| c.value_changed? }
+
+  delegate :encrypt_and_sign, :decrypt_and_verify, to: :encryptor
+  
+  KEY = ActiveSupport::KeyGenerator.new(
+    ENV.fetch("SECRET_KEY_BASE")
+  ).generate_key(
+    ENV.fetch("ENCRYPTION_SERVICE_SALT"),
+    ActiveSupport::MessageEncryptor.key_len
+  ).freeze
+  
+  private_constant :KEY
+  
+  def decrypt
+    decrypt_and_verify(self.value)
+  end
+
+  private
+  
+  def encrypt
+    self.value = encrypt_and_sign(self.value)
+  end
+
+  def encryptor
+    ActiveSupport::MessageEncryptor.new(KEY)
+  end
 end
