072c6fbbde8a04c93d70655980f18df2458de15e
William Jackson <williampj@gmail.com>
2020-11-13 19:49:27 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resource :user, except: [:new, :edit]
+  resource :user, except: %i[new, edit]
   resources :bridges
 
   post 'login', to: 'sessions#create'
