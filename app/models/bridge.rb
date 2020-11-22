ef80a02a0d9c858dbf8a2b59b850411164227191
William Jackson <williampj@gmail.com>
2020-11-21 21:31:33 -0600

    added safe operator

--- app/models/bridge.rb
+++ app/models/bridge.rb
@@ -43,8 +43,10 @@ class Bridge < ApplicationRecord
   accepts_nested_attributes_for :headers, :environment_variables
 
   def add_event_info
-    completed_at = events.filter(&:completed_at).min { |a, b| b.completed_at <=> a.completed_at }[:completed_at]
-    attributes.merge({ 'eventCount' => events.count, 'completedAt' => completed_at })
+    completed_at = events&.filter(&:completed_at)
+                         &.min { |a, b| b.completed_at <=> a.completed_at }
+                         &.fetch(:completed_at)
+    attributes.merge({ 'eventCount' => events.count, 'completedAt' => completed_at || '' })
   end
 
   private
