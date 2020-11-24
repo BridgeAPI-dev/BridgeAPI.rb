124bd0d12b2bfa43a364eeabe979a6964c374e0a
= <awcrotwell@gmail.com>
2020-11-24 08:41:00 -0500

    Sidekiq eb hooks

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -13,6 +13,12 @@ Sidekiq.configure_server do |config|
   end
 
   if Rails.env == 'production'
-    config.redis = { host: 'bridgeapi-production-redis.zocofw.0001.use2.cache.amazonaws.com' }
+    config.redis = { url: 'bridgeapi-production-redis.zocofw.0001.use2.cache.amazonaws.com:6379/1' }
+  end
+end
+
+Sidekiq.configure_client do |config|
+  if Rails.env == 'production'
+    config.redis = { url: 'bridgeapi-production-redis.zocofw.0001.use2.cache.amazonaws.com:6379/1' }
   end
 end
